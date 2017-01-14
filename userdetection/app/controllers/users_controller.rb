600165255e9e8cd5db542ad038c2a33256125f90
shubham-amura <shubham@amuratech.com>
2017-01-14 19:31:09 +0530

    Initial setup

---
+++ userdetection/app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+
+
+  def index
+  end
+  def create
+    @user = User.new(user_params)
+    byebug
+
+    auth = {
+    cloud_name: "shuboid-cloud",
+    api_key: "955113512438869",
+    api_secret:"2LNTfxCktq1T78v6UN-na7eqWZc"
+    }
+    temp = Cloudinary::Uploader.upload(params[:user][:profile_image].tempfile, auth)
+    @user[:profile_image_url] = temp["url"]
+
+    temp =  Cloudinary::Uploader.upload(params[:user][:license_image].tempfile, auth)
+    @user[:license_image_url] = temp["url"]
+
+    if @user.save!
+      render 'index'
+    end
+  end
+
+
+  def user_params
+    params.require(:user).permit(:first_name,:last_name,:age,:gender)
+  end
+
+end
--- userdetection/app/models/user.rb
+++ userdetection/app/models/user.rb
@@ -1,3 +1,13 @@
 class User
   include Mongoid::Document
+  include Mongoid::Attributes::Dynamic
+
+
+
+field :first_name , type: String
+field :last_name, type: String
+field :age, type: Integer
+field :gender , type: String
+field :profile_image_url, type:  String
+field :license_image_url , type: String
 end
